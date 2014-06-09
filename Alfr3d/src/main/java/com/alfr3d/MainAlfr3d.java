fa4ff2603b10346429038ec95b09cac3626db0dc
armageddion <armageddion@gmail.com>
2014-06-09 16:30:10 -0400

    added setting: Node.js vs. CGI

--- Alfr3d/src/main/java/com/alfr3d/MainAlfr3d.java
+++ Alfr3d/src/main/java/com/alfr3d/MainAlfr3d.java
@@ -5,12 +5,24 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.StatusLine;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 
 public class MainAlfr3d extends Activity {
     public final static String EXTRA_MESSAGE = "com.test2.MESSAGE";
@@ -59,9 +71,118 @@ public class MainAlfr3d extends Activity {
         // Do something in response to button
         Intent intent = new Intent(this, CallResponse.class);
         String message = "Blink";
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
+        //intent.putExtra(EXTRA_MESSAGE, message);
+        //startActivity(intent);
+
+        SharedPreferences mySharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        String alfr3d_url= mySharedPreferences.getString("alfr3d_url_preference","url not set");
+        String full_alfr3d_call = alfr3d_url+"/cgi-bin/alfr3d.cgi?command="+message;
+
+        // curl: "http://alfr3d.no-ip.org/cgi-bin/test2.py?command=Blink"
+        if (!full_alfr3d_call.substring(0,7).equals("http://"))
+        {
+            full_alfr3d_call = "http://"+full_alfr3d_call;
+        }
+
+        final String finalCall = full_alfr3d_call;
+
+        new Thread() {
+            public void run() {
+                try{
+                    HttpClient httpclient = new DefaultHttpClient();
+                    HttpResponse response;
+                    String responseString = null;
+                    try {
+                        response = httpclient.execute(new HttpGet(finalCall));
+                        StatusLine statusLine = response.getStatusLine();
+                        if(statusLine.getStatusCode() == HttpStatus.SC_OK){
+                            ByteArrayOutputStream out = new ByteArrayOutputStream();
+                            response.getEntity().writeTo(out);
+                            out.close();
+                            responseString = out.toString();
+                        } else{
+                            //Closes the connection.
+                            response.getEntity().getContent().close();
+                            throw new IOException(statusLine.getReasonPhrase());
+                        }
+                    } catch (ClientProtocolException e) {
+                        //TODO Handle problems..
+                    } catch (IOException e) {
+                        //TODO Handle problems..
+                    }
+                }
+                catch (Exception e) {
+                    Log.e("tag", e.getMessage());
+                }
+            }
+        }.start();
     }
+
+    public void sendButtonBlink(View view) {
+        sendButtonCommand("Blink");
+    }
+
+    public void sendButtonWelcome(View view) {
+        sendButtonCommand("welcomehome");
+    }
+
+    public void sendButtonCommand(String Command) {
+        // Do something in response to button
+        Intent intent = new Intent(this, CallResponse.class);
+        String message = Command;
+
+        SharedPreferences mySharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        String alfr3d_url= mySharedPreferences.getString("alfr3d_url_preference","url not set");
+        String full_alfr3d_call = alfr3d_url+"/cgi-bin/alfr3d.cgi?command="+message;
+
+        boolean node_enabled = mySharedPreferences.getBoolean("node_enabled",false);
+        if (node_enabled == true)
+        {
+            //TODO Finish this bit
+            full_alfr3d_call = alfr3d_url+"/complete this bit when you have it working in node";
+        }
+
+
+        // curl: "http://alfr3d.no-ip.org/cgi-bin/test2.py?command=Blink"
+        if (!full_alfr3d_call.substring(0,7).equals("http://"))
+        {
+            full_alfr3d_call = "http://"+full_alfr3d_call;
+        }
+
+        final String finalCall = full_alfr3d_call;
+
+        new Thread() {
+            public void run() {
+                try{
+                    HttpClient httpclient = new DefaultHttpClient();
+                    HttpResponse response;
+                    String responseString = null;
+                    try {
+                        response = httpclient.execute(new HttpGet(finalCall));
+                        StatusLine statusLine = response.getStatusLine();
+                        if(statusLine.getStatusCode() == HttpStatus.SC_OK){
+                            ByteArrayOutputStream out = new ByteArrayOutputStream();
+                            response.getEntity().writeTo(out);
+                            out.close();
+                            responseString = out.toString();
+                        } else{
+                            //Closes the connection.
+                            response.getEntity().getContent().close();
+                            throw new IOException(statusLine.getReasonPhrase());
+                        }
+                    } catch (ClientProtocolException e) {
+                        //TODO Handle problems..
+                    } catch (IOException e) {
+                        //TODO Handle problems..
+                    }
+                }
+                catch (Exception e) {
+                    Log.e("tag", e.getMessage());
+                }
+            }
+        }.start();
+    }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
