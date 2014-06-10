808c33229219d5bb84ab1d555b385b7b0c083804
armageddion <armageddion@gmail.com>
2014-06-10 16:17:00 -0400

    Full refresh

--- Alfr3d/src/main/java/com/alfr3d/SplashActivity.java
+++ Alfr3d/src/main/java/com/alfr3d/SplashActivity.java
@@ -2,9 +2,11 @@ package com.alfr3d;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
 
 public class SplashActivity extends Activity {
 
@@ -12,6 +14,13 @@ public class SplashActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        // attempt at animated Splash screen
+        ImageView AnimationView = (ImageView) findViewById(R.id.AnimView);
+        AnimationView.setBackgroundResource(R.drawable.animation);
+        AnimationDrawable SplashAnimation = (AnimationDrawable) AnimationView.getBackground();
+
+        SplashAnimation.start();
 
         Thread welcomeThread = new Thread(){
 
